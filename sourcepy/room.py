4a00afd233f5fb1417eae2d948e5dc641bb9cd9a
User-Hrsat <User-Hrsat@github.com>
2019-04-01 19:15:59 +0800

    change langegan

---
+++ sourcepy/room.py
@@ -0,0 +1,8 @@
+def frontdoor():
+    print ("There is a villa.Do you want go to it?\nHave a choice[\'y\' or \'n\']:")
+    choice = input()
+
+    if choice == 'y':
+        print ("Welcome!")
+    else:
+        print ("Bye")
