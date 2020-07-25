254679b5cfbf6fd8f85263522de2ba62cf044ac2
User-Hrsat <15937712794@163.com>
2020-07-25 20:38:27 +0800

    🚫💩

---
+++ main.py
@@ -0,0 +1,3 @@
+import room
+
+room.frontdoor()
---
+++ room.py
@@ -0,0 +1,7 @@
+class room():
+    
+    def frontdoor():
+        print ("There is a villa.Do you want go to it?\nHave a choice[\'y\' or \'n\']:")
+
+    def livingroom():
+        print ("There is livingroom.\nThere are garage and kitchen.\nHave a choice[\'g\' or \'k\', \'b\' to back]")
